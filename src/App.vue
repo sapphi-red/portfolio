6ce35310702462e0340adb60315e266cc13d6a23
sapphi-red <green@sapphi.red>
2020-09-17 22:53:49 +0900

    Add: vue-router

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,11 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3.0 + Vite" />
+  <router-view />
 </template>
 
 <script lang="ts">
-import HelloWorld from '/@/components/HelloWorld.vue'
 import { defineComponent } from 'vue'
+
 export default defineComponent({
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+  name: 'App'
 })
 </script>
--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,8 @@
 import { createApp } from 'vue'
+import router from './router'
 import App from './App.vue'
 import './index.scss'
 
-createApp(App).mount('#app')
+const app = createApp(App)
+app.use(router)
+app.mount('#app')
