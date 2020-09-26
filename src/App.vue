59a3cc60f59558c2fafebe46eb66795ae3527fb6
sapphi-red <green@sapphi.red>
2020-09-26 23:24:49 +0900

    Update: default font color

--- src/App.vue
+++ src/App.vue
@@ -16,3 +16,7 @@ export default defineComponent({
   }
 })
 </script>
+
+<style lang="scss">
+@import './styles/global';
+</style>
--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,6 @@
 import { createApp } from 'vue'
 import router from './router'
 import App from './App.vue'
-import './index.scss'
 
 import '@purge-icons/generated'
 
