c4f99bd8344fa030998d8709bacc4d388cd7d256
sapphi-red <green@sapphi.red>
2020-10-04 15:24:15 +0900

    Update: Contests

--- src/pages/Experiences.vue
+++ src/pages/Experiences.vue
@@ -1,6 +1,21 @@
 <template>
   <page-title>Experiences</page-title>
   <section-title>Contests</section-title>
+  <ul>
+    <li>
+      <a href="https://icttoracon.net/archives/8369">
+        ICTトラブルシューティングコンテスト2019 チームstohm 本選9位
+      </a>
+    </li>
+    <li>
+      <a href="https://nttcom.connpass.com/event/163460/">
+        学生向けN-ISUCON 1位
+      </a>
+    </li>
+    <li>
+      <a href="https://trap.jp/post/1114/">ISUCON10 本選3位</a>
+    </li>
+  </ul>
   <section-title>Internships</section-title>
   <section-title>Books</section-title>
   <section-title>OSS Contributions</section-title>
