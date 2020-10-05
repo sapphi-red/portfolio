6dec5acb5615577bd4ea89c18c0f4221343ea358
sapphi-red <green@sapphi.red>
2020-10-05 10:01:09 +0900

    Update: Internships

--- src/pages/Experiences.vue
+++ src/pages/Experiences.vue
@@ -3,6 +3,7 @@
   <section-title>Contests</section-title>
   <list :data="contests" />
   <section-title>Internships</section-title>
+  <list :data="internships" />
   <section-title>Books</section-title>
   <section-title>OSS Contributions</section-title>
 </template>
@@ -24,6 +25,21 @@ const contests: Data[] = [
   },
   { name: 'ISUCON10 チーム「がんもどき」 本選3位' }
 ]
+
+const internships: Data[] = [
+  {
+    name: '2019/08: モイ株式会社 ツイキャスサマーインターン2019',
+    href: 'https://twitcasting.tv/internship2019.php',
+    children: [
+      { name: '機械学習を利用したライブ配信UIを3人チームで2週間で製作。' }
+    ]
+  },
+  {
+    name: '2020/08: 株式会社メルカリ Online Summer Internship for Gophers 2020',
+    href: 'https://mercan.mercari.com/articles/22800/',
+    children: [{ name: 'Go言語のLintツールを1週間で製作。' }]
+  },
+  { name: '2020/09～: 株式会社マネーフォワード' }
 ]
 
 export default defineComponent({
@@ -34,7 +50,7 @@ export default defineComponent({
     List
   },
   setup() {
-    return { contests }
+    return { contests, internships }
   }
 })
 </script>
