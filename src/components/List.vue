f54ea7f22672744a4770b5c2bc26a4acbcc9266e
sapphi-red <green@sapphi.red>
2020-10-05 09:36:50 +0900

    Update: List

---
+++ src/components/List.vue
@@ -0,0 +1,34 @@
+<template>
+  <ul>
+    <li v-for="datum in data" :key="datum.name">
+      <a v-if="datum.href !== undefined" :href="datum.href">{{ datum.name }}</a>
+      <template v-else>{{ datum.name }}</template>
+      <list v-if="datum.children.length > 0" :data="datum.children" />
+    </li>
+  </ul>
+</template>
+
+<script lang="ts">
+import { defineComponent, PropType } from 'vue'
+
+export interface Data {
+  name: string
+  href?: string
+  children: Data[]
+}
+
+export default defineComponent({
+  name: 'List',
+  props: {
+    data: {
+      type: Array as PropType<Data[]>,
+      required: true
+    }
+  }
+})
+</script>
+
+<style lang="scss" module>
+.container {
+}
+</style>
--- src/pages/Experiences.vue
+++ src/pages/Experiences.vue
@@ -1,21 +1,7 @@
 <template>
   <page-title>Experiences</page-title>
   <section-title>Contests</section-title>
-  <ul>
-    <li>
-      <a href="https://icttoracon.net/archives/8369">
-        ICTトラブルシューティングコンテスト2019 チームstohm 本選9位
-      </a>
-    </li>
-    <li>
-      <a href="https://nttcom.connpass.com/event/163460/">
-        学生向けN-ISUCON 1位
-      </a>
-    </li>
-    <li>
-      <a href="https://trap.jp/post/1114/">ISUCON10 本選3位</a>
-    </li>
-  </ul>
+  <list :data="contests" />
   <section-title>Internships</section-title>
   <section-title>Books</section-title>
   <section-title>OSS Contributions</section-title>
@@ -25,12 +11,31 @@
 import { defineComponent } from 'vue'
 import PageTitle from '/@/components/PageTitle.vue'
 import SectionTitle from '/@/components/SectionTitle.vue'
+import List, { Data } from '../components/List.vue'
+
+const contests: Data[] = [
+  {
+    name: 'ICTトラブルシューティングコンテスト2019 チームstohm 本選9位',
+    href: 'https://icttoracon.net/archives/8369',
+    children: []
+  },
+  {
+    name: '学生向けN-ISUCON 1位',
+    href: 'https://nttcom.connpass.com/event/163460/',
+    children: []
+  },
+  { name: 'ISUCON10 チーム「がんもどき」 本選3位', children: [] }
+]
 
 export default defineComponent({
   name: 'Experiences',
   components: {
     PageTitle,
-    SectionTitle
+    SectionTitle,
+    List
+  },
+  setup() {
+    return { contests }
   }
 })
 </script>
