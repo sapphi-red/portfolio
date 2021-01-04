c3d0b6c4d665675a9f4efaf8393a39198a44f704
sapphi-red <green@sapphi.red>
2021-01-04 15:39:11 +0900

    Revert "Update: brotli compression"
    This reverts commit 605f21ca12f6acec623ec5b91d6548c7510d089b.

--- vite.config.ts
+++ vite.config.ts
@@ -2,8 +2,6 @@ import { UserConfig } from 'vite'
 import path from 'path'
 import PurgeIcons from 'vite-plugin-purge-icons'
 import { ViteToml } from 'vite-plugin-toml'
-// @ts-expect-error no d.ts but ok
-import brotli from 'rollup-plugin-brotli'
 
 const srcPath = path.resolve(__dirname, 'src').replace(/\\/g, '/')
 
@@ -16,10 +14,7 @@ const config: UserConfig = {
       additionalData: `@import "${srcPath}/styles/common.scss";`
     }
   },
-  plugins: [PurgeIcons(), ViteToml()],
-  rollupInputOptions: {
-    pluginsPostBuild: [brotli()]
-  }
+  plugins: [PurgeIcons(), ViteToml()]
 }
 
 export default config
