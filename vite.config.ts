55e153d8625959abf6cd00122c1d8c90c462eeaa
sapphi-red <green@sapphi.red>
2021-01-07 13:17:37 +0900

    Update: preload css

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,4 @@
-import { UserConfig } from 'vite'
+import { UserConfig, Plugin } from 'vite'
 // @ts-expect-error: prevent installing @types/node
 import path from 'path'
 import Vue from '@vitejs/plugin-vue'
@@ -8,6 +8,22 @@ import { ViteToml } from 'vite-plugin-toml'
 declare const process: { env: Record<string, string> }
 declare const __dirname: string
 const srcPath = path.resolve(__dirname, 'src').replace(/\\/g, '/')
+
+const preloadPlugin: Plugin = {
+  name: 'preload',
+  transformIndexHtml(html, ctx) {
+    if (ctx.server) return
+
+    const cssMatch = [...html.matchAll(/<link rel="stylesheet" href="(.+)">/g)]
+    if (cssMatch.length > 0) {
+      const cssPreloads = cssMatch
+        .map(m => m[1])
+        .map(src => `<link rel="preload" href="${src}" as="style">`)
+      html = html.replace(/<meta charset=.+\/>/, `$&\n${cssPreloads}`)
+    }
+    return html
+  }
+}
 
 const config: UserConfig = {
   alias: {
@@ -25,7 +41,8 @@ const config: UserConfig = {
       ssr: !!process.env.VITE_SSG
     }),
     PurgeIcons(),
-    ViteToml()
+    ViteToml(),
+    preloadPlugin
   ]
 }
 
