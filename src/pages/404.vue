8bdc5cd0ecbff3ab95d5c23cdfcf5694c32a7202
sapphi-red <green@sapphi.red>
2021-01-07 11:50:43 +0900

    Update: 404 page

---
+++ src/pages/404.vue
@@ -0,0 +1,54 @@
+<template>
+  <div :class="$style.container">
+    <page-title>404</page-title>
+    <img src="/magatama-green.svg" :class="$style.img" />
+    <div :class="$style.link">
+      <router-link to="/">Go to Top</router-link>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+import PageTitle from '/@/components/UI/PageTitle.vue'
+
+export default defineComponent({
+  name: 'About',
+  components: {
+    PageTitle
+  }
+})
+</script>
+
+<style lang="scss" module>
+.container {
+  text-align: center;
+}
+.img {
+  animation: rotate 1.5s linear infinite;
+}
+.link {
+  margin-top: 10%;
+}
+
+@keyframes rotate {
+  0% {
+    transform: rotate(0);
+  }
+  50% {
+    transform: rotate(360deg);
+  }
+  60% {
+    transform: rotate(360deg);
+  }
+  75% {
+    transform: rotate(360deg) translateY(-10%);
+  }
+  90% {
+    transform: rotate(360deg);
+  }
+  100% {
+    transform: rotate(360deg);
+  }
+}
+</style>
