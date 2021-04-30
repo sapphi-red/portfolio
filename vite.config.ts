b7e8162a7df867afdbadcf63417a5e0f1bbee7f2
sapphi-red <green@sapphi.red>
2021-04-30 11:45:56 +0900

    Chore: Add @types/node

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,9 @@
 import { UserConfig } from 'vite'
-// @ts-expect-error: prevent installing @types/node
 import path from 'path'
 import Vue from '@vitejs/plugin-vue'
 import PurgeIcons from 'vite-plugin-purge-icons'
 import { ViteToml } from 'vite-plugin-toml'
 
-declare const process: { env: Record<string, string> }
-declare const __dirname: string
 const srcPath = path.resolve(__dirname, 'src').replace(/\\/g, '/')
 
 const config: UserConfig = {
