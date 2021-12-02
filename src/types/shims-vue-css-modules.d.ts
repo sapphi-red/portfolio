00ed61868cb699b76df887c67ec2e52da7f307a9
sapphi-red <green@sapphi.red>
2021-12-02 14:00:54 +0900

    Chore: use vite/client type

--- src/types/shims-vue-css-modules.d.ts
+++
@@ -1,8 +0,0 @@
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-import { ComponentCustomProperties } from 'vue'
-
-declare module '@vue/runtime-core' {
-  interface ComponentCustomProperties {
-    $style: { [className: string]: string }
-  }
-}
