1ae79fd9c171ba0f3d5208f9565d7bd25a07519e
sapphi-red <green@sapphi.red>
2022-09-02 12:01:01 +0900

    Refactor: migrate from import.meta.globEager

--- src/components/Works/composables/useWorksImageOrFallback.ts
+++ src/components/Works/composables/useWorksImageOrFallback.ts
@@ -3,14 +3,15 @@ import { computed } from 'vue'
 import type { Work } from '/@/assets/works'
 import noimgSvgUrl from '/@/assets/img/noimg.svg'
 
-const worksImgUrls = import.meta.globEager<{ default: string }>(
-  '../../../assets/img/works/*.png'
+const worksImgUrls = import.meta.glob<string>(
+  '../../../assets/img/works/*.png',
+  { eager: true, import: 'default' }
 )
 
 const useWorksImageOrFallback = (work: Ref<Work>): ComputedRef<string> => {
   return computed(
     () =>
-      worksImgUrls[`../../../assets/img/works/${work.value.img}`]?.default ??
+      worksImgUrls[`../../../assets/img/works/${work.value.img}`] ??
       noimgSvgUrl
   )
 }
